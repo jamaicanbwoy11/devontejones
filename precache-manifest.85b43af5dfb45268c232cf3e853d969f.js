c72f06f6af34ec309e3a0a00ce5317b9ada33632
Devonte Jones <devontejs36@gmail.com>
2021-10-14 23:31:53 -0700

    Updates

---
+++ precache-manifest.85b43af5dfb45268c232cf3e853d969f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6260ae47d5842ff32671b558fc8f716",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4eefd74fc81ac7dcf7e",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "ba83964888bdd294c8cf",
+    "url": "/static/css/main.32dc93a0.chunk.css"
+  },
+  {
+    "revision": "a4eefd74fc81ac7dcf7e",
+    "url": "/static/js/2.2d3a5209.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.2d3a5209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba83964888bdd294c8cf",
+    "url": "/static/js/main.d7292e27.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
